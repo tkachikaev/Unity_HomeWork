b20fb3e277ee58e5be1f2c98ae34573d755aff57
Timur <54122011+tkachikaev@users.noreply.github.com>
2020-09-09 00:31:47 +0300

    HomeWork_4_ver_1.5

---
+++ HomeWork_4/Assets/Scripts/GameCore.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using System;
+using System.Transactions;
+
+
+public class GameCore : MonoBehaviour
+{
+    // Ссылка на счетчик
+    [SerializeField] private Text count;
+
+    #region Ответы на вопросы
+    [SerializeField] private string question1 = "1945";
+    [SerializeField] private string question2 = "4";
+    [SerializeField] private string question3 = "белый";
+    [SerializeField] private string question4 = "200";
+    [SerializeField] private string question5 = "android";
+    #endregion
+
+    #region Счетчик
+    private int yes = 0;
+    private int no = 0;
+    #endregion
+
+    public void onClickQuestion1(InputField inputFild)
+    {
+        if (inputFild.text.Equals(question1.ToString())) yes++;
+        else no++;
+    }
+
+    public void onClickQuestion2(InputField inputFild)
+    {
+        if (inputFild.text.Equals(question2.ToString())) yes++;
+        else no++;
+    }
+
+    public void onClickQuestion3(InputField inputFild)
+    {
+        if (inputFild.text.Equals(question3.ToString())) yes++;
+        else no++;
+    }
+
+    public void onClickQuestion4(InputField inputFild)
+    {
+        if (inputFild.text.Equals(question4.ToString())) yes++;
+        else no++;
+    }
+
+    public void onClickQuestion5(InputField inputFild)
+    {
+        if (inputFild.text.Equals(question5.ToString())) yes++;
+        else no++;
+    }
+
+    public void winLose()
+    {
+        count.text = $"Верно: {yes}\nНе верно: {no}";
+    }
+
+    public void ResetCount()
+    {
+        yes = 0;
+        no = 0;
+    }
+}
---
+++ HomeWork_4/Assets/Scripts/StateScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateScript : MonoBehaviour
+{
+    [SerializeField] private GameObject FirstScreen;
+    [SerializeField] private GameObject DisabledScreen;
+    private GameObject curentScreen;
+
+    private void Start()
+    {
+        FirstScreen.SetActive(true);
+        curentScreen = FirstScreen;
+        if (DisabledScreen != null)
+        {
+            DisabledScreen.SetActive(false);
+        };
+    }
+
+    public void ChangeState(GameObject state)
+    {
+        if (curentScreen != null)
+        {
+            curentScreen.SetActive(false);
+            state.SetActive(true);
+            curentScreen = state;
+        }
+    }
+}
