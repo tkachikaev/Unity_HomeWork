068c5a8ae71f801bb89f096e88698654c3fd25e0
Timur <54122011+tkachikaev@users.noreply.github.com>
2020-12-06 16:24:03 +0300

    HW_9_2-3w

---
+++ HomeWork_9/Assets/Scripts/Boll.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boll : MonoBehaviour
+{
+    public float ImpulceForce;
+    private Rigidbody _rigidbody;
+
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        _rigidbody.AddForce(Vector3.forward * ImpulceForce, ForceMode.Impulse);
+    }
+}
---
+++ HomeWork_9/Assets/Scripts/GravitySphere.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravitySphere : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        other.gameObject.GetComponent<Rigidbody>().useGravity = false;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        other.gameObject.GetComponent<Rigidbody>().useGravity = true;
+    }
+}
